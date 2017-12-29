3dcc5bafe22ad5a2dfa30b27f8eae37b51993a07
Madhubala-E <31853326+Madhubala-E@users.noreply.github.com>
2017-12-29 15:01:48 +0530

    serverinstall.ps1

--- installation.ps1
+++ installation.ps1
@@ -1,192 +1,23 @@
-<# Installing additional Components like
-	IIS, Tomcat, Websphere, MySQL softwares and
-	Google Chrome browser,7Zip, Tomcat utilities
-	in Azure windows based vm images
-#>
+<# To verify the server manager status for DHCP and DNS #>
+Get-WindowsFeature
 
-<# Import environment variables #>
- param
-	(
-		[bool]$InstallIIS=1,
-		[bool]$InstallTomcat=0,
-		[bool]$InstallWebSphere=0,
-		[bool]$InstallMySQL=0,
-		[bool]$InstallGooglechrome=0,
-		[bool]$Install7Zip=0,
-		[string]$WebServerPort='',
-		[string]$WebServerPackage=''
-	)
+<# Install DNS server role #>
+Add-WindowsFeature -Name DNS -IncludeManagementTools
 
-	try {
-	<# Altering the time zone of the machine #>
-		cd $env:WINDIR\system32\
-		tzutil /s 'India Standard Time'
-	}
-	catch
-	{
-		Write-Output "Unable to set the time zone"
-		$ErrorMessage = $_.Exception.Message
-		$FailedItem = $_.Exception.ItemName
-		Write-Output $ErrorMessage
-	}
+Get-WindowsFeature -Name *DNS*
 
-	try {
-		<# Install Chocolatey  #>
-		iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-		refreshenv
-	}
-	catch
-	{
-		Write-Output "Unable to install the Chocolatey"
-		$ErrorMessage = $_.Exception.Message
-		$FailedItem = $_.Exception.ItemName
-		Write-Output $ErrorMessage
-	}
+<# Adding DNS server primary zone #>
+Add-DnsServerPriamryZone -Name zirrusgavs.com -zonefile zirrusgavs.com.DNS -DynamicUpdate NonsecureAndSecure
 
-	try {
-		<# VC++ runtimes needed as dependencies for other installations #>
-		choco install vcredist-all -y
-	}
-	catch
-	{
-		Write-Output "Unable to install VC++ redistributables"
-		$ErrorMessage = $_.Exception.Message
-		$FailedItem = $_.Exception.ItemName
-		Write-Output $ErrorMessage
-	}
+<# Install DHCP server role #>
+Install-WindowsFeature -Name DHCP -IncludeManagementTools
 
-	try {
-		<# SQL Server Management Studio #>
-		choco install sql-server-management-studio -y
-	}
-	catch
-	{
-		Write-Output "Unable to install SQL Server management Studio"
-		$ErrorMessage = $_.Exception.Message
-		$FailedItem = $_.Exception.ItemName
-		Write-Output $ErrorMessage
-	}
-		
-	try {
-		<# Install IIS Server #>
-		Install-WindowsFeature -Name web-server -IncludeManagementTools
-		Install-WindowsFeature -Name Web-Asp-Net45
+Get-NetIPConfiguration
 
-		<# Configure the IIS server #>
-		Set-WebBinding -Name 'Default Web Site' -BindingInformation "*:80:" -PropertyName Port -Value 8080
-		netsh advfirewall firewall add rule name="HTTP Web Application" dir=in action=allow protocol=TCP localport=8080
-	}
-	catch
-	{
-		Write-Output "Unable to install IIS Server"
-		$ErrorMessage = $_.Exception.Message
-		$FailedItem = $_.Exception.ItemName
-		Write-Output $ErrorMessage
-	}
-
-	try {
-		#If($InstallIIS -eq 1)
-		#{
-			<# Install the IIS server #>
-			#Install-WindowsFeature -Name web-server -IncludeManagementTools
-			#Install-WindowsFeature -Name Web-Asp-Net45
-
-			<# Configure the Firewall #>
-			#Set-WebBinding -Name 'Default Web Site' -BindingInformation "*:80:" -PropertyName Port -Value $WebServerPort
-			#netsh advfirewall firewall add rule name="HTTP Web Application" dir=in action=allow protocol=TCP localport=$WebServerPort
-
-			If($WebServerPackage -ne '')
-			{
-				# Folders
-		#        New-Item -ItemType Directory c:\temp
-		#        New-Item -ItemType Directory c:\sites
-
-				# Download app package
-		#        Invoke-WebRequest  $WebServerPackage -OutFile c:\temp\app.zip
-		#        Expand-Archive C:\temp\app.zip c:\sites
-
-				# Configure iis
-		#       Remove-WebSite -Name "Default Web Site"
-				#Set-ItemProperty IIS:\AppPools\DefaultAppPool\ managedRuntimeVersion ""
-		#       New-Website -Name "Application" -Port 80 -PhysicalPath C:\sites\ -ApplicationPool DefaultAppPool & iisreset
-			}
-		#}
-	}
-	catch
-	{
-		Write-Output "Unable to deploy the packages to IIS Server"
-		$ErrorMessage = $_.Exception.Message
-		$FailedItem = $_.Exception.ItemName
-		Write-Output $ErrorMessage
-	}
-
-	#<# Import environment variables #>
-	# $InstallIIS = $env:installiis
-	# $InstallTomcat = $env:installtomcat
-	# $InstallWebSphere = $env:installwebsphere
-	# $InstallMySQL = $env:installmysql
-	# $InstallGooglechrome = $env:installgooglechrome
-	# $Install7Zip = $env:install7zip
-	# $WebServerPort = $env:webserverport
-	# $WebServerPackage = $env:webserverpackage
-
-	choco feature enable -n allowGlobalConfirmation
+<# Adding DHCP server scope #>
+Add-DhcpServerv4Scope -Name DHCPClient -StartRange 10.10.0.1 -EndRange 10.10.0.4 -SubnetMask 255.255.255.0
 
 
-	try {
-		if($InstallGooglechrome -eq 1)
-		{
-			<# Google Chrome Browser #>
-			choco install googlechrome -y
-		}
-	}
-	catch
-	{
-		Write-Output "Unable to install Google Chrome browser"
-		$ErrorMessage = $_.Exception.Message
-		$FailedItem = $_.Exception.ItemName
-		Write-Output $ErrorMessage
-	}
 
-	
 
-	try {
-		If($InstallMySQL -eq 1)
-		{
-			<# 7zip application #>
-			choco install 7zip -y
 
-			<# MySQL #>
-			choco install mysql -y
-		}
-	}
-	catch
-	{
-		Write-Output "Unable to install MySQL server"
-		$ErrorMessage = $_.Exception.Message
-		$FailedItem = $_.Exception.ItemName
-		Write-Output $ErrorMessage
-	}
-
-        try {
-
-               If($InstallTomcat -eq 1)
-               {
-                    <# Tomcat application #>
-                     choco install tomcat -y
-                    
-               }
-
-             }
-       
-        catch 
-        {
-                 
-		Write-Output "Unable to install tomcat"
-		$ErrorMessage = $_.Exception.Message
-		$FailedItem = $_.Exception.ItemName
-		Write-Output $ErrorMessage
-
-          }
-               
-        
