19b4192f9dbb5c476083e1874c20c4fa67ae6a95
Madhubala-E <31853326+Madhubala-E@users.noreply.github.com>
2018-02-13 10:59:03 +0000

    installation1.ps1

---
+++ installation1.ps1
@@ -0,0 +1,10 @@
+﻿
+$New_msi_path= "C:\Program Files\GCareAgent\GCareAgent.msi"
+    
+    $msifile= $New_msi_path
+    $arguments= ' /qn /l*v .\install_SQLSysClrTypes.txt' 
+Start-Process `
+     -file  $msifile `
+     -arg $arguments `
+     -passthru | wait-process
+
